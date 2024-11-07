8dbbdd0a75ba817d8e82fb04cb236aaf802a35f4
Nuoya Jiang <nuoyajiang@g.ucla.edu>
2024-11-06 23:33:43 -0500

    fix entrypoint in model-training

--- src/model-training/entrypoint.sh
+++ src/model-training/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-export GOOGLE_APPLICATION_CREDENTIALS='/no_ship/data-service-account.json'
+#export GOOGLE_APPLICATION_CREDENTIALS='/no_ship/data-service-account.json'
 
 # Execute the tensorization script with arguments
-python tensorizing.py \
-    -sb mini-215-multiclass-car-bucket \
-    -c class_label.csv \
-    -db mini-tensor-bucket \
-    -o ./output
+pipenv run python model_training.py
