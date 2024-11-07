a9fa9b71a3338bfb28e8b37ff0f6def21fd29ea6
Nuoya Jiang <nuoyajiang@g.ucla.edu>
2024-11-07 00:01:29 -0500

    finish model deployment

---
+++ src/model-deployment/docker-shell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# exit immediately if a command exits with a non-zero status
+set -e
+
+# Define some environment variables
+export IMAGE_NAME="215_data_pipeline_model_deployment"
+export BASE_DIR=$(pwd)
+export LOCAL_MOUNT_DIR="./no_ship"
+
+# Build the image based on the Dockerfile
+docker build --no-cache -t $IMAGE_NAME -f Dockerfile .
+#docker build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
+
+# Run the container
+docker run --rm \
+--name $IMAGE_NAME \
+-e GOOGLE_APPLICATION_CREDENTIALS='/no_ship/ai-service-account.json' \
+-v $LOCAL_MOUNT_DIR:/no_ship \
+-v "./":/app \
+-ti $IMAGE_NAME
