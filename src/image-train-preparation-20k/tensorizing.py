32abfd4073d307d957efeb0ad1f3cdd211510811
xinyi-wang02 <xinyi_wang2@hms.harvard.edu>
2024-11-18 16:47:34 -0500

    stratified sampling data download

--- src/image-train-preparation-20k/tensorizing.py
+++ src/image-train-preparation-20k/tensorizing.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from google.cloud import storage
 from io import BytesIO
 from PIL import Image
@@ -22,8 +23,13 @@ def download_data(source_bucket_name, csv_blob_name, local_image_dir, local_csv_
     # Read CSV to get image names
     df = pd.read_csv(local_csv_path)
 
-    # Randomly sample `sample_size` (default 20k) images
-    df_sampled = df.sample(n=sample_size, random_state=42)
+    # Stratified sampling to sample `sample_size` (default 20k) images
+    df_sampled, _ = train_test_split(
+        df, 
+        train_size=sample_size, 
+        stratify=df['label'], 
+        random_state=42
+    )
     
     # Ensure the local image directory exists
     os.makedirs(local_image_dir, exist_ok=True)
