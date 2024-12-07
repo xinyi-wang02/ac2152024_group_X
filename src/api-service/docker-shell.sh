efdc966e754cc30a6585f6e6f7234ddd972f5bd0
Nuoya Jiang <98131931+NuoyaJiang@users.noreply.github.com>
2024-12-07 03:38:55 -0500

    fixed API and frontend (#9)
    * fixed API and frontend
    
    * fixed linting of API and frontend

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -4,12 +4,12 @@
 set -e
 
 # Define some environment variables
-export IMAGE_NAME="215_data_pipeline_model_deployment"
+export IMAGE_NAME="215_data_pipeline_model_deployment_api"
 export BASE_DIR=$(pwd)
 export LOCAL_MOUNT_DIR="./no_ship"
 
 # Build the image based on the Dockerfile
-# docker build --no-cache -t $IMAGE_NAME -f Dockerfile .
+docker build --no-cache -t $IMAGE_NAME -f Dockerfile .
 #docker build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
 
 # Run the container
