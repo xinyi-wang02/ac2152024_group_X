7c1fe452490fdc4de6475e85656d5be538d8db7b
xinyi-wang02 <xinyi_wang2@hms.harvard.edu>
2024-12-10 13:24:36 -0500

    fix action test

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-source model_training/.env
+#source model_training/.env
 pipenv run wandb login $WANDB_KEY
 cd tests
 pipenv run python -m coverage run --source=../ --omit=../src/tests/* -m pytest ./
